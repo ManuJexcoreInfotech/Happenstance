02ed66fa22eeb7ec63816381147246f80f9bbec0
Manu jexcore <web_dev_2@shubantecgnologies.com>
2017-10-27 16:26:38 +0530

    Update

--- www/js/app.js
+++ www/js/app.js
@@ -62,37 +62,43 @@ angular.module('app', [
                     // org.apache.cordova.statusbar required
                     StatusBar.styleDefault();
                 }
+				onDeviceReady();
 				
-				var push = PushNotification.init({
-					android: {
-					},
-					browser: {
-						pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-					},
-					ios: {
-						alert: "true",
-						badge: "true",
-						sound: "true"
-					},
-					windows: {}
-				});
+				function onDeviceReady(){
 
-				push.on('registration', function(data) {
-					alert(data.registrationId);
-				});
+					var push = PushNotification.init({
+						android: {
+							senderID: "871704003795",
+							sound: true,
+							vibrate: true
+						},
+						browser: {},
+						ios: {
+							alert: "true",
+							badge: "true",
+							sound: "true"
+						},
+						windows: {}
+					});
 
-				push.on('notification', function(data) {
-					// data.message,
-					// data.title,
-					// data.count,
-					// data.sound,
-					// data.image,
-					// data.additionalData
-				});
+					push.on('registration', function(data) {
+						//data.registrationId;
+					}); 
 
-				push.on('error', function(e) {
-					// e.message
-				});				
+					push.on('notification', function(data) {
+						// data.message,
+						// data.title,
+						// data.count,
+						// data.sound,
+						// data.image,
+						// data.additionalData
+					   
+					});
+
+					push.on('error', function(e) {
+						// e.message
+					});
+				   }		
             });
             $rootScope.backHome = function () {
 
